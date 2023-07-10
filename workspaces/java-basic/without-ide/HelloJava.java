749714f3067b405ae09f402de9e53ee40a197981
instructor <instructor@example.com>
2023-07-10 15:41:04 +0900

    update code

---
+++ workspaces/java-basic/without-ide/HelloJava.java
@@ -0,0 +1,11 @@
+public class HelloJava {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello, Java Programming World !!!");
+		System.out.println("This is my first java program !!!");
+		System.out.println("Bye ~~~");
+	
+	}
+
+}

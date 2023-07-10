a094c01c1b2543ca840b9b924a9c8dc9d5c74a24
instructor <instructor@example.com>
2023-07-10 17:21:03 +0900

    update code

---
+++ workspaces/java-basic/Ex02TypeAndVariables/src/TypeAndVariables.java
@@ -0,0 +1,22 @@
+
+public class TypeAndVariables {
+
+	public static void main(String[] args) {
+		
+		// 1. 변수 만들기 : 
+		//    자료형 변수이름; 
+		//    자료형 변수이름 = 10;
+		//    자료형 변수이름1 = 10, 변수이름2 = 20; 
+		
+		int i1 = 10; // 이름이 i1인 4byte 정수형 변수 만들기 
+		System.out.printf("i1 = %d\n", i1); // \n : enter 
+		// i1 = 11.11; // 오류 : 정수형 변수에 실수형 값을 저장하면 오류
+		
+		double d1;
+		d1 = 11.23;
+		System.out.printf("d1 = %f\n", d1);
+		
+
+	}
+
+}

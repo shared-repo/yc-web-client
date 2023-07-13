2e01e1421cdebdd87b7fb0c7ee8afc18ad4eebdf
instructor <instructor@example.com>
2023-07-13 16:27:12 +0900

    update code

--- workspaces/java-basic/Ex06Method/src/Lotto.java
+++ workspaces/java-basic/Ex06Method/src/Lotto.java
@@ -20,22 +20,13 @@ public class Lotto {
 			switch (selection) {
 			case "1":
 				// 0. 6개의 숫자를 저장할 배열 만들기
-				int[] numbers = new int[6];
+				int[] numbers; // = new int[6]; // selectBasicNumbers 메서드에서 new 처리
 				double avg = 0;
 				do {
+					
 					// 1. 6개의 숫자 뽑기 ( 조건 : 1 ~ 45, Random, 중복X ) + 배열에 저장
-					for (int i = 0; i < numbers.length; i++) {
-						numbers[i] = (int)(Math.random() * 45) + 1;
-						
-						// 중복 검사 반복문 ( 현재 뽑힌 i번째 번호와 0 ~ i-1번째 번호 비교 )
-						for (int j = 0; j < i; j++) {
-							if (numbers[i] == numbers[j]) {
-								i--; // i 반복문에서 i++로 넘어가는 것을 상쇄하기 위해 --처리
-								// i = -1; // i 반복문에서 i를 0으로 만들어서 처음부터 다시 뽑기
-								break;
-							}				
-						}
-					}			
+					numbers = selectBasicNumbers(); // 이 호출이 가능하도록 메서드 구현하세요
+					
 					// 2. 뽑힌 숫자의 평균 계산
 					int sum = 0;
 					for (int i = 0; i < numbers.length; i++) {
@@ -62,6 +53,23 @@ public class Lotto {
 		}
 	}
 
+	public static int[] selectBasicNumbers() {
+		int[] numbers = new int[6];
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = (int)(Math.random() * 45) + 1;
+			
+			// 중복 검사 반복문 ( 현재 뽑힌 i번째 번호와 0 ~ i-1번째 번호 비교 )
+			for (int j = 0; j < i; j++) {
+				if (numbers[i] == numbers[j]) {
+					i--; // i 반복문에서 i++로 넘어가는 것을 상쇄하기 위해 --처리
+					// i = -1; // i 반복문에서 i를 0으로 만들어서 처음부터 다시 뽑기
+					break;
+				}				
+			}
+		}
+		return numbers;
+	}
+	
 	public static void showNumbers(int[] numbers, double avg) {
 		System.out.print("[당첨 예상 번호]:");
 		for (int i = 0; i < numbers.length; i++) {

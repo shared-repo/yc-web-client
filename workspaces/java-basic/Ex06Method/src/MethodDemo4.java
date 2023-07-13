e50f02ac9c66dae3b3a54364e67edaf9fb1db23e
instructor <instructor@example.com>
2023-07-13 12:29:04 +0900

    update code

--- workspaces/java-basic/Ex06Method/src/MethodDemo4.java
+++ workspaces/java-basic/Ex06Method/src/MethodDemo4.java
@@ -3,21 +3,42 @@ public class MethodDemo4 {
 	
 	public static void main(String[] args) {
 		
-		int d1 = 10, d2 = 20;
-		int result = sum(d1, d2);
+		// int[] ar = new int[] { 1, 2, 3 };
+		int[] ar = { 1, 2, 3 };
+		useArrayParam(ar);
+		System.out.println("=================");
+		useArrayParam(new int[] { 10, 20, 30});
+		System.out.println("=================");
+		// useArrayParam({ 100, 200, 300}); // 오류 : { 초기화 목록 } 형식은 변수 선언 구문에서만 사용
 		
+		//////////////////////////////////////////////////////
+		
+		int result = 0;
+		result = sum(10, 20);
+		System.out.println(result);
+		result = sum(10, 20, 30);
+		System.out.println(result);
+		result = sum(10, 20, 30, 40);
+		System.out.println(result);
+		result = sum(10, 20, 30, 40, 50);
 		System.out.println(result);
 		
+		System.out.printf("%s%d%s%d", 1, 2, 3, 4);
 	}
 	
-	
-	// 두 정수를 더하는 함수 만들기
-	// void return type : 반환 값 없음
-	public static int sum(int d1, int d2) {	
-		
-		int result = d1 + d2;
-		return result; // return; --> 함수 종료, return 값; --> 함수를 종료하고 호출한 곳으로 값 반환
-		
+	public static void useArrayParam(int[] ar) {
+		for (int v : ar) {
+			System.out.println(v);
+		}
 	}
+	
+	public static int sum(int...values) { // ...은 불특정 개수의 전달인자를 받는 배열 전달인자 변수
+		int result = 0;
+		for (int v : values) {
+			result += v;
+		}
+		return result;
+	}
+	
 
 }

fbfa382bbcb71da2efe683a28b8ef8da5d2b843f
instructor <instructor@example.com>
2023-07-14 12:15:27 +0900

    update code

--- workspaces/java-basic/Ex06Method/src/BaseballGame.java
+++ workspaces/java-basic/Ex06Method/src/BaseballGame.java
@@ -14,10 +14,15 @@ public class BaseballGame {
 			case "1":
 				// 1. 컴퓨터 숫자 선택 (0 ~ 9, Random, 3개) --> 배열에 저장
 				int[] comNums = selectComNums();
-				System.out.printf("%d %d %d", comNums[0], comNums[1], comNums[2]);
+				System.out.printf("%d %d %d\n", comNums[0], comNums[1], comNums[2]);
 				
 				// 2. 사용자 숫자 선택 (0~9, 입력, 3개) --> 배열에 저장
+				
+				int[] userNums = inputUserNums();
+				System.out.printf("%d %d %d\n", userNums[0], userNums[1], userNums[2]);
+						
 				// 3. 컴퓨터 숫자와 사용자 선택 비교 후 결과 판정 ( S, B, O ) --> 3S가 나오면 Win, 아니면 2부터 다시, 10회 반복되면 Lose
+				
 				break;
 				
 			case "2": break;
@@ -30,6 +35,18 @@ public class BaseballGame {
 			}
 			System.out.println();
 		}
+	}
+
+	private static int[] inputUserNums() {
+		System.out.print("숫자를 선택하세요 ( 숫자1 숫자2 숫자3 ) : ");
+		String line = scanner.nextLine();
+		String[] sNums = line.split(" "); // 지정된 문자열(여기서는 공백)을 사용해서 문자열 분해하고 배열로 반환 : "1 2 3" --> { "1", "2", "3" }
+		// System.out.printf("%s %s %s\n", sNums[0], sNums[1], sNums[2]);
+		int[] userNums = new int[3];
+		for (int i = 0; i < userNums.length; i++) {
+			userNums[i] = Integer.parseInt(sNums[i]); //Integer.parseInt("10"); : "10" -> 10
+		}
+		return userNums;
 	}
 
 	private static int[] selectComNums() {

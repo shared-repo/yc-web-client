a61dd6b760fa9b1f5eba34f5bc0a998adcda354a
instructor <instructor@example.com>
2023-07-14 10:48:01 +0900

    update code

---
+++ workspaces/java-basic/Ex06Method/src/BaseballGame.java
@@ -0,0 +1,15 @@
+
+public class BaseballGame {
+
+	public static void main(String[] args) {
+		
+		// 1. 컴퓨터 숫자 선택 (0 ~ 9, Random, 3개) --> 배열에 저장
+		
+		// 2. 사용자 숫자 선택 (0~9, 입력, 3개) --> 배열에 저장
+		// 3. 컴퓨터 숫자와 사용자 선택 비교 후 결과 판정 ( S, B, O ) --> 3S가 나오면 Win, 아니면 2부터 다시, 10회 반복되면 Lose
+		
+		// 사용자가 원하는 동안 반복
+
+	}
+
+}

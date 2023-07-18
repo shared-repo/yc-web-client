9910b32effd342327f3d2bbff2386f0ca7045af4
instructor <instructor@example.com>
2023-07-18 10:25:36 +0900

    update code

--- workspaces/java-basic/Ex07OOP/src/ToDo.java
+++ workspaces/java-basic/Ex07OOP/src/ToDo.java
@@ -8,13 +8,15 @@ public class ToDo {
 	private java.util.Date regDate;
 	
 	public ToDo() {
-		// do nothing
+		this.regDate = new java.util.Date();
 	}	
-	public ToDo(int no, String title, String content, Date regDate) {
+	public ToDo(int no, String title, String content /*, Date regDate */) {
+		this(); // 같은 클래스의 다른 생성자 메서드 호출 (여기서는 전달인자 없는 기본 생성자 메서드 호출)
 		this.no = no;
 		this.title = title;
 		this.content = content;
-		this.regDate = regDate;
+		// this.regDate = regDate;
+		// this.regDate = new java.util.Date();
 	}
 
 	public int getNo() {
--- workspaces/java-basic/Ex07OOP/src/ToDoApp.java
+++ workspaces/java-basic/Ex07OOP/src/ToDoApp.java
@@ -16,17 +16,31 @@ public class ToDoApp {
 			case "1": // 등록 처리
 				ToDo toDo = inputNewToDo(); 
 				// 4. ToDo 인스턴스를 할 일 관리 배열에 추가
+				toDo.setNo(nextPosition+1);
 				toDos[nextPosition] = toDo;
 				nextPosition++; // 다음에 등록할 위치 변경
 				break;
-			case "2": 
+			case "2": // 목록 보기
 				if (nextPosition == 0) {
 					System.out.println("등록된 할 일이 없습니다.");
 				} else {
 					showAllToDos();
 				}
 				break;
-			case "3": break;
+			case "3": // 검색 
+				// 검색어 입력
+				System.out.print("검색어를 입력하세요 : ");
+				String searchWord = scanner.nextLine();
+				// 입력된 내용이 포함된 ToDo 찾기 (반복문)
+				for (int i = 0; i < nextPosition; i++) {
+					String title = toDos[i].getTitle();
+					// if (title.equals(selection)) { // 문자열의 내용 비교는 equals 사용 ( 비교연산자 사용 X )
+					if (title.contains(searchWord)) { // constains(str) : str이 포함되어 있다면 true 아니면 false 
+						// 결과 표시
+						System.out.println(toDos[i].info());
+					}
+				}
+				break;
 			case "4": break;
 			case "5": break;
 			case "9": 
@@ -56,11 +70,10 @@ public class ToDoApp {
 		String title = scanner.nextLine();
 		System.out.print("할 일 내용 : ");
 		String content = scanner.nextLine();
-		java.util.Date now = new java.util.Date(); // new java.util.Date(); 현재 시간으로 Date 객체 초기화
 		// 3. 입력 데이터를 toDo 인스턴스에 저장
 		toDo.setTitle(title);
 		toDo.setContent(content);
-		toDo.setRegDate(now);
+		
 		return toDo;
 	}	
 

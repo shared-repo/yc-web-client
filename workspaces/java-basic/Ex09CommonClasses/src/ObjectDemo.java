361aceb925c3b5152a5702bbc1034cefbf4090db
instructor <instructor@example.com>
2023-07-19 10:12:25 +0900

    update code

---
+++ workspaces/java-basic/Ex09CommonClasses/src/ObjectDemo.java
@@ -0,0 +1,22 @@
+
+
+class Item {
+//	int no;
+//	String name;
+//	
+//	public Item() {}
+//	public Item(int no, String name) {
+//		this.no= no;
+//		this.name = name;
+//	}
+}
+
+public class ObjectDemo {
+
+	public static void main(String[] args) {
+		
+		//
+
+	}
+
+}

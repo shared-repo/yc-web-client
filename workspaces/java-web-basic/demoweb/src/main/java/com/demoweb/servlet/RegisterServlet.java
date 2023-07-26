94b54275fd7b85b9a72ea994f2c002c2b617381d
instructor <instructor@example.com>
2023-07-26 14:56:25 +0900

    update code

--- workspaces/java-web-basic/demoweb/src/main/java/com/demoweb/servlet/RegisterServlet.java
+++ workspaces/java-web-basic/demoweb/src/main/java/com/demoweb/servlet/RegisterServlet.java
@@ -2,7 +2,10 @@ package com.demoweb.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,6 +21,19 @@ import javax.servlet.http.HttpServletResponse;
 public class RegisterServlet extends HttpServlet {
 
 	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		//1. 요청 데이터 읽기 (여기서는 생략 - 데이터 없음)		
+		//2. 요청 처리 (여기서는 생략 - 처리 내용 없음)
+		//3. JSP에서 읽을 수 있도록 Request 객체에 데이터 저장 (생략)
+		
+		//4. 응답 컨텐츠 생상 ( JSP에서 처리 - forward로 이동)
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/account/register.jsp");
+		dispatcher.forward(req, resp);
+		
+	}
+	
+	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
 		// 1. 요청 데이터 읽기 : req.getParameter("이름")
@@ -30,10 +46,14 @@ public class RegisterServlet extends HttpServlet {
 		// 2. 처리
 		System.out.printf("[%s][%s][%s][%s]\n", memberId, passwd, confirm, email);
 
-		// 3. 응답컨텐츠 생산
-		resp.setContentType("text/html;charset=utf-8");
-		PrintWriter out = resp.getWriter();
-		out.printf("<h1>[%s][%s][%s][%s]</h1>", memberId, passwd, confirm, email);
+		// 3. 응답컨텐츠 생산 ( 여기서는 다른 작업 영역으로 redirect로 이동 )
+//		resp.setContentType("text/html;charset=utf-8");
+//		PrintWriter out = resp.getWriter();
+//		out.printf("<h1>[%s][%s][%s][%s]</h1>", memberId, passwd, confirm, email);
+		
+		resp.sendRedirect("/demoweb/index.jsp");
+		
+		
 		
 		
 	}

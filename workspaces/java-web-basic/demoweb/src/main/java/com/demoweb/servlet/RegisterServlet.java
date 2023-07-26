1f8dfe29bad616bdbc48fca421764e776bcec0c5
instructor <instructor@example.com>
2023-07-26 10:19:31 +0900

    update code

--- workspaces/java-web-basic/demoweb/src/main/java/com/demoweb/servlet/RegisterServlet.java
+++ workspaces/java-web-basic/demoweb/src/main/java/com/demoweb/servlet/RegisterServlet.java
@@ -1,6 +1,7 @@
 package com.demoweb.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 // 2. @WebServlet 매핑
 // 3. doGet or doPost 구현
 
-@WebServlet(urlPatterns = { "/account/register.action" }, ) // 서버 코드에서는 절대 경로에 웹애플리케이션 이름 사용 X
+@WebServlet(urlPatterns = { "/account/register.action" } ) // 서버 코드에서는 절대 경로에 웹애플리케이션 이름 사용 X
 public class RegisterServlet extends HttpServlet {
 
 	@Override
@@ -26,8 +27,13 @@ public class RegisterServlet extends HttpServlet {
 		String confirm = req.getParameter("confirm");
 		String email = req.getParameter("email");
 		
+		// 2. 처리
 		System.out.printf("[%s][%s][%s][%s]\n", memberId, passwd, confirm, email);
-		
+
+		// 3. 응답컨텐츠 생산
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out = resp.getWriter();
+		out.printf("<h1>[%s][%s][%s][%s]</h1>", memberId, passwd, confirm, email);
 		
 		
 	}

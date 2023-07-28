77618bca2ad75d3a087d9fd3b07b3af9eddc9e5f
instructor <instructor@example.com>
2023-07-28 16:13:26 +0900

    update code

---
+++ workspaces/java-web-basic/examples-web/src/main/java/com/exampleweb/filter/MyFilter1.java
@@ -0,0 +1,46 @@
+package com.exampleweb.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// Filter 만들기
+// 1. Filter 인터페이스 구현
+// 2. 메서드 재정의 (특히 doFilter)
+// 3. 필터 등록 ( web.xml 또는 @Filter annotation )
+
+public class MyFilter1 implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest)request;
+		String url = req.getRequestURI(); // 현재 처리 중인 요청 경로
+		
+		System.out.println(url + " 요청이 처리되고 있습니다.");
+		
+		if (url.contains("08-01.")) {
+			HttpServletResponse resp = (HttpServletResponse)response;
+			resp.sendRedirect("/examples-web/"); // 다음 필터 또는 서블릿 또는 JSP는 처리되지 않음
+		} else {
+			chain.doFilter(request, response); // 다음 필터 또는 서블릿 또는 JSP 호출
+		}
+	}
+	
+
+}
+
+
+
+
+
+
+
+

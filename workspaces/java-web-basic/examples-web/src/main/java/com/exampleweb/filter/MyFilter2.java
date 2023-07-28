09e847a331a7740b4051b9435202cb15c1f6b7a9
instructor <instructor@example.com>
2023-07-28 16:51:05 +0900

    update code

--- workspaces/java-web-basic/examples-web/src/main/java/com/exampleweb/filter/MyFilter2.java
+++ workspaces/java-web-basic/examples-web/src/main/java/com/exampleweb/filter/MyFilter2.java
@@ -7,6 +7,7 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,7 +16,8 @@ import javax.servlet.http.HttpServletResponse;
 // 2. 메서드 재정의 (특히 doFilter)
 // 3. 필터 등록 ( web.xml 또는 @Filter annotation )
 
-public class MyFilter1 implements Filter {
+@WebFilter(urlPatterns = { "*.jsp" })
+public class MyFilter2 implements Filter {
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -24,9 +26,9 @@ public class MyFilter1 implements Filter {
 		HttpServletRequest req = (HttpServletRequest)request;
 		String url = req.getRequestURI(); // 현재 처리 중인 요청 경로
 		
-		System.out.println(url + " 요청이 처리되고 있습니다 (in filter1).");
+		System.out.println(url + " 요청이 처리되고 있습니다 (in filter2).");
 		
-		if (url.contains("08-01.")) { // 요청 경로에 "08-01." 문자열이 포함되어 있으면
+		if (url.contains("08-02.")) { // 요청 경로에 "08-01." 문자열이 포함되어 있으면
 			HttpServletResponse resp = (HttpServletResponse)response;
 			resp.sendRedirect("/examples-web/"); // 다음 필터 또는 서블릿 또는 JSP는 처리되지 않음
 		} else {

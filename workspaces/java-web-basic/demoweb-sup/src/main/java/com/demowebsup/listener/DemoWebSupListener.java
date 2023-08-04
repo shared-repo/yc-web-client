605963f5904e6d1626f5870e1205dacd40a79f6b
instructor <instructor@example.com>
2023-08-04 11:40:38 +0900

    update code

--- workspaces/java-web-basic/demoweb-sup/src/main/java/com/demowebsup/listener/DemoWebSupListener.java
+++ workspaces/java-web-basic/demoweb-sup/src/main/java/com/demowebsup/listener/DemoWebSupListener.java
@@ -1,5 +1,7 @@
 package com.demowebsup.listener;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import javax.servlet.ServletContext;
@@ -38,7 +40,26 @@ public class DemoWebSupListener implements ServletContextListener, HttpSessionLi
 	}
 	// 웹 애플리케이션 정상 종료할 때 호출
 	public void contextDestroyed(ServletContextEvent sce) {
+		
+		// 파일에 저장할 게시글 데이터를 application(ServletContext) 객체에서 읽어오기
+		ServletContext application = sce.getServletContext();
+		ArrayList<BoardDto> boards = (ArrayList<BoardDto>)application.getAttribute("boards");
+		
 		// 게시글 데이터를 파일에 저장
+		FileOutputStream fos = null;
+		ObjectOutputStream oos = null;
+		try {
+			String path = application.getRealPath("/WEB-INF/boards.dat"); // 웹경로 -> 컴퓨터경로
+			fos = new FileOutputStream(path);
+			oos = new ObjectOutputStream(fos);
+			oos.writeObject(boards);
+		} catch (Exception ex) {
+			ex.printStackTrace(); // 현재 예외 정보를 콘솔에 출력하는 명령
+		} finally {
+			// oos.close(); // 예외처리 필수 구문
+			try { oos.close(); } catch (Exception ex) {}
+			try { fos.close(); } catch (Exception ex) {}
+		}
 	}
 
 }

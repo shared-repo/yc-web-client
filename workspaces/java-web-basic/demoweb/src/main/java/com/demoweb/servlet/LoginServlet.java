268e2e8cb4188f3a2a6dee91bdc7d17b2eba70c5
instructor <instructor@example.com>
2023-08-07 11:13:08 +0900

    update code

--- workspaces/java-web-basic/demoweb/src/main/java/com/demoweb/servlet/LoginServlet.java
+++ workspaces/java-web-basic/demoweb/src/main/java/com/demoweb/servlet/LoginServlet.java
@@ -34,6 +34,7 @@ public class LoginServlet extends HttpServlet {
 		
 		// 2. 요청 처리 ( 로그인 처리 )
 		// System.out.printf("[%s][%s]\n", memberId, passwd); // 테스트 코드
+		// 저장된 회원가입정보에서 사용자 정보 조회 및 로그인 결정
 		if (memberId.equals("iamuser") && passwd.equals("1234")) { // 로그인 성공
 			// 로그인 처리 - 세션에 로그인 정보 저장
 			HttpSession session = req.getSession(); // 요청 객체로부터 세션 객체 가져오기
--- workspaces/java-web-basic/demoweb/src/main/java/com/demoweb/servlet/RegisterServlet.java
+++ workspaces/java-web-basic/demoweb/src/main/java/com/demoweb/servlet/RegisterServlet.java
@@ -3,14 +3,18 @@ package com.demoweb.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.demoweb.dto.MemberDto;
 
 // 서블릿 만들기
 // 1. HttpServlet 상속
@@ -36,15 +40,26 @@ public class RegisterServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		// 1. 요청 데이터 읽기 : req.getParameter("이름")
+		// 1-1. 요청 데이터 읽기 : req.getParameter("이름")
 		req.setCharacterEncoding("utf-8");
 		String memberId = req.getParameter("memberId");
 		String passwd = req.getParameter("passwd");
 		String confirm = req.getParameter("confirm");
 		String email = req.getParameter("email");
 		
-		// 2. 처리
-		System.out.printf("[%s][%s][%s][%s]\n", memberId, passwd, confirm, email);
+		// 1-2. MemberDto 객체에 요청 데이터 저장
+		MemberDto member = new MemberDto();
+		member.setMemberId(memberId);
+		member.setPasswd(passwd);
+		member.setEmail(email);
+		member.setRegDate(new Date());
+		
+		// 2. 처리 (회원가입정보 저장)
+		// System.out.printf("[%s][%s][%s][%s]\n", memberId, passwd, confirm, email);
+		ServletContext application = req.getServletContext(); // jsp의 application 기본 객체와 같은 객체
+		ArrayList<MemberDto> members = (ArrayList<MemberDto>)application.getAttribute("members");
+		// members에 회원 데이터 저장하는 코드 작성
+		members.add(member);
 
 		// 3. 응답컨텐츠 생산 ( 여기서는 다른 작업 영역으로 redirect로 이동 )
 //		resp.setContentType("text/html;charset=utf-8");

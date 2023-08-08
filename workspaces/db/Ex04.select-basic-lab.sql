34c932069a23a2f9f849b92aebb1bf76d7cca7da
instructor <instructor@example.com>
2023-08-08 16:11:58 +0900

    update code

--- workspaces/db/Ex04.select-basic-lab.sql
+++ workspaces/db/Ex04.select-basic-lab.sql
@@ -20,3 +20,15 @@ WHERE custid = 1;
 SELECT COUNT(*) "박지성 고객의 구매 도서 수량"
 FROM orders
 WHERE custid = 1;
+
+-- 마당서점 도서의 총 개수
+SELECT COUNT(*) "총 도서 개수"
+FROM book;
+
+-- 마당서점에 도서를 출고하는 출판사의 총 수 (중복 제거 필요)
+SELECT publisher
+FROM book; -- 중복 출판사 확인
+
+SELECT COUNT(DISTINCT publisher) "총 출판사 수"
+FROM book;
+

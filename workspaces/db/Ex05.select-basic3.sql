6652117638487130f47fde1bbc2eeb01448456ea
instructor <instructor@example.com>
2023-08-09 10:20:44 +0900

    update code

--- workspaces/db/Ex05.select-basic3.sql
+++ workspaces/db/Ex05.select-basic3.sql
@@ -53,4 +53,39 @@ ON c.custid = o.custid
 INNER JOIN book b
 ON o.bookid = b.bookid;
 
+-- 가격이 20000원인 도서를 구매한 고객의 이름, 도서명, 주문 정보 조회
+SELECT c.custid, c.name, b.bookname, o.saleprice, o.orderdate
+FROM customer c, orders o, book b
+WHERE c.custid = o.custid AND o.bookid = b.bookid AND b.price = 20000;
+
+SELECT c.custid, c.name, b.bookname, o.saleprice, o.orderdate
+FROM customer c
+INNER JOIN orders o
+ON c.custid = o.custid
+INNER JOIN book b
+ON o.bookid = b.bookid
+WHERE b.price = 20000;
+
+-- 모든 고객의 고객별 주문 실적 조회 (고객아이디, 고객이름, 주문 건수, 주문 총액)
+SELECT c.custid, c.name, COUNT(o.saleprice) 주목건수, SUM(o.saleprice) 주문총액
+FROM customer c, orders o
+WHERE c.custid = o.custid
+GROUP BY c.custid, c.name;
+
+SELECT * FROM customer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

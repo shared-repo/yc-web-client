f48f0faf637ae8902b6b046b5e46a3743c7fc97d
instructor <instructor@example.com>
2023-08-10 16:57:05 +0900

    update code

---
+++ workspaces/db/Ex11.dml-lab.sql
@@ -0,0 +1,24 @@
+-- 작업 데이터베이스 변경
+USE madang;
+
+-- (1) 새로운 도서 (‘스포츠 세계’, ‘대한미디어’, 10000원)이 마당서점에 입고되었다. 
+--      삽입이 안 될 경우 필요한 데이터가 더 있는지 찾아보자.
+INSERT INTO NEWBOOK (BOOKNAME, PUBLISHER, PRICE)
+VALUES ('스포츠 세계', '대한미디어', 10000);
+
+SELECT * FROM NEWBOOK;
+
+-- (2) ‘삼성당’에서 출판한 도서를 삭제해야 한다.
+START TRANSACTION;
+
+DELETE FROM BOOK
+WHERE PUBLISHER = '삼성당';
+
+SELECT * FROM BOOK;
+
+ROLLBACK;
+
+SELECT * FROM BOOK;
+
+-- (3) ‘이상미디어’에서 출판한 도서를 삭제해야 한다. 삭제가 안 될 경우 원인을 생각해보자.
+-- (4) 출판사 ‘대한미디어’가 ‘대한출판사’로 이름을 바꾸었다.

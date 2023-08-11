53ae54a1210100944edd523904664a4edd079fd9
instructor <instructor@example.com>
2023-08-11 16:51:11 +0900

    update code

--- workspaces/java-web-basic/demoweb/src/main/java/com/demoweb/dao/AccountDao.java
+++ workspaces/java-web-basic/demoweb/src/main/java/com/demoweb/dao/AccountDao.java
@@ -1,5 +1,19 @@
 package com.demoweb.dao;
 
+import com.demoweb.dto.MemberDto;
+
 public class AccountDao {
+	
+	// 회원가입에 사용할 메서드
+	public void insertMember(MemberDto member) {
+		
+	}
+	
+	
+	// 로그인에 사용할 메서드
+	public MemberDto selectMemberByIdAndPasswd(String id, String passwd) {
+		
+		return null;
+	}
 
 }

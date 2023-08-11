1218ca418b028e34af522034c37533486260654d
instructor <instructor@example.com>
2023-08-11 14:23:30 +0900

    update code

---
+++ workspaces/java-web-basic/jdbc-basic/src/EmployeeDao.java
@@ -0,0 +1,13 @@
+
+// Dao 클래스 : 데이터베이스 연동 기능을 구현한 클래스
+
+import java.util.ArrayList;
+
+public class EmployeeDao {
+	
+	public ArrayList<EmployeeDto> selectEmployeesByGender(String gender) {
+		
+		return null;
+	}
+
+}

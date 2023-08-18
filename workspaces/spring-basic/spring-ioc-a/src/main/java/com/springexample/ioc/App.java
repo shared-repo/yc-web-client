439f221d1d9396cd8e6c509dfa725d70da3178c9
instructor <instructor@example.com>
2023-08-18 14:00:07 +0900

    update code

--- workspaces/spring-basic/spring-ioc-a/src/main/java/com/springexample/ioc/App.java
+++ workspaces/spring-basic/spring-ioc-a/src/main/java/com/springexample/ioc/App.java
@@ -11,6 +11,9 @@ public class App {
 		ServiceConsumer serviceConsumer = context.getBean("serviceConsumer", ServiceConsumer.class);
 		serviceConsumer.doSomething();
 		
+		ServiceConsumer serviceConsumer2 = context.getBean("serviceConsumer", ServiceConsumer.class);
+		serviceConsumer2.doSomething(); // singleton이면 위의 객체와 같은 번호 출력 prototype이면 다른 번호 출력
+		
 		context.close();
 
 	}
--- workspaces/spring-basic/spring-ioc-a2/src/main/java/com/springexample/ioc/AppContextConfig.java
+++ workspaces/spring-basic/spring-ioc-a2/src/main/java/com/springexample/ioc/AppContextConfig.java
@@ -2,12 +2,14 @@ package com.springexample.ioc;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration // app-context.xml과 같이 bean 설정을 수행하는 클래스로 지정
 public class AppContextConfig {
 	
 	@Bean 	// xml 설정의 <bean id="serviceConsumer" class="...MyServiceConsumer"와 같은 효과
 			// 메서드 이름이 bean의 id가 됩니다.
+	@Scope("singleton") // or prototype
 	public MyServiceConsumer serviceConsumer() {
 		// return new MyServiceConsumer();
 		MyServiceConsumer serviceConsumer = new MyServiceConsumer();
@@ -16,7 +18,8 @@ public class AppContextConfig {
 		return serviceConsumer;
 	}
 	
-	@Bean
+	@Bean(initMethod = "init", destroyMethod = "destroy")
+	@Scope("singletone")
 	public MyMessageService messageService() {
 		return new MyMessageService();
 	}

0e1a5545aef41137780302b852f9364fe870a174
instructor <instructor@example.com>
2023-08-22 11:48:21 +0900

    update code

--- workspaces/spring-basic/demoweb/src/main/java/com/demoweb/controller/LoginController.java
+++ workspaces/spring-basic/demoweb/src/main/java/com/demoweb/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.demoweb.controller;
 
+import com.demoweb.dao.AccountDao;
 import com.demoweb.dto.HandleResultDto;
+import com.demoweb.dto.MemberDto;
 
 public class LoginController {
 
@@ -11,37 +13,18 @@ public class LoginController {
 		if (method.equals("get")) {
 			return new HandleResultDto(false, "account/login.jsp", null);
 		} else {
-			return new HandleResultDto(false, "account/login.jsp", null);
+			MemberDto member1 = (MemberDto)formData;
+			AccountDao dao = new AccountDao();
+			MemberDto member = dao.selectMemberByIdAndPasswd(member1.getMemberId(), member1.getPasswd());
+			if (member != null) {
+				return new HandleResultDto(true, "/demoweb/home.action", member);
+			} else {
+				return new HandleResultDto(false, "account/login.jsp", null);	
+			}
 		}
 		
 	}
 	
-//	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		// 1. 요청 데이터 읽기
-//		req.setCharacterEncoding("utf-8");
-//		String memberId = req.getParameter("memberId");
-//		String passwd = req.getParameter("passwd");
-//		
-//		// 2. 요청 처리 ( 로그인 처리 )
-//		// System.out.printf("[%s][%s]\n", memberId, passwd); // 테스트 코드
-//		// 저장된 회원가입정보에서 사용자 정보 조회 및 로그인 결정
-//		AccountDao dao = new AccountDao();
-//		MemberDto member = dao.selectMemberByIdAndPasswd(memberId, passwd);
-//			
-//		if (member != null) { // 로그인 성공
-//			// 로그인 처리 - 세션에 로그인 정보 저장
-//			HttpSession session = req.getSession(); // 요청 객체로부터 세션 객체 가져오기
-//			session.setAttribute("loginuser", memberId); // 세션에 데이터 저장
-//			
-//			// 4. 일련의 작업이 끝났으면 redirect로 이동
-//			resp.sendRedirect("/demoweb/home.action"); // sendRedirect는 메서드를 종료하지 않습니다.
-//			return; // 메서드 종료
-//		}
-//		
-//		// 3. JSP에게 전달할 데이터를 request객체에 저장
-//		req.setAttribute("loginfail", true);
-//		doGet(req, resp); // 다시 로그인 페이지로 forward 이동
-//	}
 }
 
 
--- workspaces/spring-basic/demoweb/src/main/java/com/demoweb/servlet/FrontControllerServlet.java
+++ workspaces/spring-basic/demoweb/src/main/java/com/demoweb/servlet/FrontControllerServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.demoweb.controller.HomeController;
 import com.demoweb.controller.LoginController;
@@ -33,8 +34,26 @@ public class FrontControllerServlet extends HttpServlet {
 			HomeController controller = new HomeController();
 			result = controller.handleRequest(method, null);
 		} else if (url.contains("/account/login.action")) {
+			
+			// 1. 요청 데이터 읽기
+			MemberDto member = null;
+			if (method.equals("post")) {
+				String memberId = req.getParameter("memberId");
+				String passwd = req.getParameter("passwd");
+				member = new MemberDto();
+				member.setMemberId(memberId);
+				member.setPasswd(passwd);
+			}
+			
 			LoginController controller = new LoginController();
-			result = controller.handleRequest(method, null);
+			result = controller.handleRequest(method, member);
+			
+			if (result.getData() != null) { // 로그인 성공
+				// 로그인 처리 - 세션에 로그인 정보 저장
+				HttpSession session = req.getSession(); // 요청 객체로부터 세션 객체 가져오기
+				session.setAttribute("loginuser", member.getMemberId()); // 세션에 데이터 저장
+			}
+			
 		} else if (url.contains("/account/register.action")) {
 			MemberDto member = null;
 			if (method.equals("post")) {

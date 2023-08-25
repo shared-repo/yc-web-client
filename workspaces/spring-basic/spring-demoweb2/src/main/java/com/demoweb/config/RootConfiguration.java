cc46706b999af45ce3b879a0989be5d3c76d1f3e
instructor <instructor@example.com>
2023-08-25 15:18:49 +0900

    update code

--- workspaces/spring-basic/spring-demoweb2/src/main/java/com/demoweb/config/RootConfiguration.java
+++ workspaces/spring-basic/spring-demoweb2/src/main/java/com/demoweb/config/RootConfiguration.java
@@ -1,8 +1,72 @@
 package com.demoweb.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.demoweb.dao.DataSourceAccountDao;
+import com.demoweb.dao.JdbcAccountDao;
+import com.demoweb.dao.JdbcTemplateAccountDao;
+import com.demoweb.service.AccountServiceImpl;
 
 @Configuration
 public class RootConfiguration {
+	
+	@Bean
+	public BasicDataSource dbcpDataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/demoweb");
+		dataSource.setUsername("devuserone");
+		dataSource.setPassword("devuserone");
+		dataSource.setMaxTotal(10);
+		dataSource.setMaxIdle(5);
+		dataSource.setMaxWaitMillis(-1);
+		
+		return dataSource;
+	}
+	
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
+		jdbcTemplate.setDataSource(dbcpDataSource()); // 의존 주입 ( bean 생성 메서드 호출 -> getBean() 과 같은 의미)
+	
+		return jdbcTemplate;
+	}
+	
+	@Bean
+	public JdbcAccountDao jdbcAccountDao() {
+		return new JdbcAccountDao();
+	}
+	
+	@Bean 
+	DataSourceAccountDao dataSourceAccountDao() {
+		DataSourceAccountDao accountDao = new DataSourceAccountDao();
+		accountDao.setDataSource(dbcpDataSource());
+		return accountDao;
+	}
+	
+	@Bean
+	public JdbcTemplateAccountDao jdbcTemplateAccountDao() {
+		JdbcTemplateAccountDao accountDao = new JdbcTemplateAccountDao();
+		accountDao.setJdbcTemplate(jdbcTemplate());
+		return accountDao;
+	}
+	
+	@Bean
+	public AccountServiceImpl accountService() {
+		AccountServiceImpl accountService = new AccountServiceImpl();
+		// accountService.setAccountDao(jdbcAccountDao());
+		// accountService.setAccountDao(dataSourceAccountDao());
+		accountService.setAccountDao(jdbcTemplateAccountDao());
+		return accountService;
+	}
 
 }
+
+
+
+
+
+
---
+++ workspaces/spring-basic/spring-demoweb2/src/main/java/com/demoweb/service/AccountServiceImpl.java
@@ -0,0 +1,27 @@
+package com.demoweb.service;
+
+import com.demoweb.dao.AccountDao;
+import com.demoweb.dao.JdbcAccountDao;
+import com.demoweb.dto.MemberDto;
+
+import lombok.Setter;
+
+public class AccountServiceImpl implements AccountService {
+	
+	@Setter
+	private AccountDao accountDao;
+	
+	@Override
+	public void register(MemberDto member) {
+		// AccountDao accountDao = new AccountDaoImpl();
+		accountDao.insertMember(member);
+	}
+	
+	@Override
+	public MemberDto findLoginMember(MemberDto member) {
+		// AccountDao accountDao = new AccountDaoImpl();
+		MemberDto loginMember = accountDao.selectMemberByIdAndPasswd(member.getMemberId(), member.getPasswd());
+		return loginMember;
+	}
+
+}

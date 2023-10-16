4cfad88b47249b6b66a31b149972b1ee2f9e3c37
instructor <instructor@example.com>
2023-10-16 11:21:03 +0900

    update code

---
+++ workspaces/spring-basic/spring-demoweb2/src/main/java/com/demoweb/dto/MemberReadBoard.java
@@ -0,0 +1,14 @@
+package com.demoweb.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class MemberReadBoard {
+
+	private String memberId;
+	private int boardNo;
+	private Date readDate;
+	
+}
